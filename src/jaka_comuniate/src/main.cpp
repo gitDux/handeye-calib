f3d58ccb28f33ca465ffeee460c2248f7be8126c
easy小风 <32471993+gitDux@users.noreply.github.com>
2021-10-15 23:36:13 +0800

    改用 JAKA-API 获取机器人状态信息

--- src/jaka_comuniate/src/main.cpp
+++ src/jaka_comuniate/src/main.cpp
@@ -1,75 +1,16 @@
 #include <iostream>
 #include "ros/ros.h"
+
 #include "jsoncpp/json/json.h"
 #include "comuniate/client.h"
 #include "sensor_msgs/JointState.h"
 #include "tf/transform_datatypes.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "JAKAZuRobot.h"
+
 using std::endl;
 using namespace std;
-
-void connect(TcpClientPtr tcpClient,string addr)
-{
-    tcpClient->setIp(addr);
-    tcpClient->setPort(10000);
-    if (tcpClient->setup())
-        ROS_INFO_STREAM("connect tcp server success");
-    else
-        ROS_ERROR_STREAM("connect tcp server failed");
-}
-
-
-Json::Value recvData(TcpClientPtr tcpclient,string ip)
-{
-    int recv_num;
-    int total_num = 0;
-    char recv_buf[8192];
-    std::string recvStr;
-
-    for (;ros::ok();)
-    {
-        if ((recv_num = recv(tcpclient->getSocket(), &recv_buf[total_num], (sizeof(recv_buf) - total_num), 0)) > 0)
-        {
-            total_num += recv_num;
-            if (total_num > 5000)
-            {
-                if (total_num >= 8192)
-                    recv_buf[total_num] = '\0';
-                else
-                    recv_buf[sizeof(recv_buf) - 1] = '\0';
-                recvStr = recv_buf;
-                break;
-            }
-        }
-        else
-        {
-            tcpclient->closeSocket();
-            connect(tcpclient,ip);
-        }
-    }
-    //Jaka's tcp protocol had been changed.
-    //The begining value is joint_actual_position now.
-    std::string::size_type begin = recvStr.find("{\"joint_actual_position\"");
-    if (begin == recvStr.npos)
-    {
-        ROS_WARN("jaka client recv error data.");
-    }
-
-    std::string::size_type end = recvStr.find("{\"joint_actual_position\"", begin + 1);
-    if (end == recvStr.npos)
-    {
-        ROS_WARN("jaka client recv error data.");
-    }
-    recvStr = recvStr.substr(begin, end);
-    Json::Reader reader;
-    Json::Value root;
-    if (!reader.parse(recvStr, root, false))
-    {
-        ROS_WARN("position parse error.");
-    }
-    return root;
-}
 
 double radu(double angl)
 {
@@ -83,7 +24,10 @@ int main(int argc, char **argv)
     ros::Publisher joint_publisher = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
     ros::Publisher jaka_pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("jaka_pose", 1);
     ros::Rate rate = ros::Rate(10);
-    TcpClientPtr tcpClient = std::make_shared<TcpClient>();
+
+    //实例API对象demo  
+	JAKAZuRobot m_robot;
+
 
     std::string ip;
     ros::param::get("/jaka_comuniate/jaka_host", ip);
@@ -91,41 +35,43 @@ int main(int argc, char **argv)
     if (ip == ""){
         ROS_ERROR("Can't get ip addr ,Please check param");
     }
-    connect(tcpClient,ip);
+    //登陆控制器  
+	m_robot.login_in(ip.c_str());
     long frame_id =0;
 
     while (ros::ok())
     {
-        Json::Value root = recvData(tcpClient,ip);
-        Json::Value pos =root["joint_actual_position"];
-        Json::Value acl_pos = root["actual_position"];
-        if(pos.size()>0){
+        RobotStatus m_status{};
+        errno_t m_flag = m_robot.get_robot_status(&m_status);
+        
+
+        if(m_flag == ERR_SUCC){
             sensor_msgs::JointState joint_state;
             joint_state.header.stamp = ros::Time::now();
             joint_state.name.resize(6);
             joint_state.position.resize(6);
             joint_state.name={"joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"};
             joint_state.position = {
-                radu(pos[0].asDouble()),
-                radu(pos[1].asDouble()),
-                radu(pos[2].asDouble()),
-                radu(pos[3].asDouble()),
-                radu(pos[4].asDouble()),
-                radu(pos[5].asDouble())
+                radu(m_status.joint_position[0]),
+                radu(m_status.joint_position[1]),
+                radu(m_status.joint_position[2]),
+                radu(m_status.joint_position[3]),
+                radu(m_status.joint_position[4]),
+                radu(m_status.joint_position[5])
                 };
             joint_publisher.publish(joint_state);
         }
 
-        if(acl_pos.size()>0)
+        if(m_flag == ERR_SUCC)
         {
             geometry_msgs::PoseStamped pose;
             frame_id++;
             pose.header.frame_id = std::to_string(frame_id);
             pose.header.stamp = ros::Time::now();
-            pose.pose.position.x = acl_pos[0].asDouble()/1000;
-            pose.pose.position.y = acl_pos[1].asDouble()/1000;
-            pose.pose.position.z = acl_pos[2].asDouble()/1000;
-            geometry_msgs::Quaternion q = tf::createQuaternionMsgFromRollPitchYaw(acl_pos[3].asDouble()*3.1415926/180,acl_pos[4].asDouble()/180.0*3.1415926,acl_pos[5].asDouble()*3.1415926/180);
+            pose.pose.position.x = m_status.cartesiantran_position[0]/1000;
+            pose.pose.position.y = m_status.cartesiantran_position[1]/1000;
+            pose.pose.position.z = m_status.cartesiantran_position[2]/1000;
+            geometry_msgs::Quaternion q = tf::createQuaternionMsgFromRollPitchYaw(m_status.cartesiantran_position[3]*3.1415926/180,m_status.cartesiantran_position[4]/180.0*3.1415926,m_status.cartesiantran_position[5]*3.1415926/180);
             pose.pose.orientation.x = q.x;
             pose.pose.orientation.y = q.y;
             pose.pose.orientation.z = q.z;
